81bbf7aad1c49e7468396a6cdf8a798fe55d9db5
Shammi athocho <159710612+athocho99kkm@users.noreply.github.com>
2024-12-09 23:43:07 +0600

    modify add_cart.php

--- Group Projects - Avengers Algorithm/TrendyTreasure/components/add_cart.php
+++ Group Projects - Avengers Algorithm/TrendyTreasure/components/add_cart.php
@@ -16,7 +16,6 @@ if(isset($_POST['add_to_cart'])){
       $image = filter_var($image, FILTER_SANITIZE_STRING);
       $qty = $_POST['qty'];
       $qty = filter_var($qty, FILTER_SANITIZE_STRING);
-
       $check_cart_numbers = $conn->prepare("SELECT * FROM `cart` WHERE name = ? AND user_id = ?");
       $check_cart_numbers->execute([$name, $user_id]);
 
